ee2883ba2853f267e822a51d76b1ac5cabf0aa02
Bruno Brasil <bruno.brasilf@gmail.com>
2021-04-20 22:44:38 -0300

    refactor: remove routes from the server file

--- src/server.ts
+++ src/server.ts
@@ -3,12 +3,4 @@ import "./database";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  return res.json({ message: "Olá NLW 05" });
-});
-
-app.post("/users", (req, res) => {
-  return res.json({ message: "Usuário salvo com sucesso" });
-});
-
 app.listen(3333, () => console.log("Server is running on port 3333"));
