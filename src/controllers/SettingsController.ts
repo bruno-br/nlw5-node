f4441906294709a00771cb41af864d4896b9adf1
Bruno Brasil <bruno.brasilf@gmail.com>
2021-04-21 10:52:29 -0300

    refactor: move repository creation to services

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,18 +1,11 @@
 import { Request, Response } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "../repositories/SettingsRepository";
+import { SettingsService } from "../services/SettingsService";
 
 export class SettingsController {
   async create(request: Request, response: Response) {
     const { chat, username } = request.body;
-    const settingsRepository = getCustomRepository(SettingsRepository);
-
-    const settings = settingsRepository.create({
-      chat,
-      username,
-    });
-    await settingsRepository.save(settings);
-
-    return response.json(settings);
+    const settingsService = new SettingsService();
+    const settings = await settingsService.create({ chat, username });
+    return response.json({ chat, username });
   }
 }
---
+++ src/services/SettingsService.ts
@@ -0,0 +1,21 @@
+import { getCustomRepository } from "typeorm";
+import { SettingsRepository } from "../repositories/SettingsRepository";
+
+interface ISettingsCreate {
+  chat: boolean;
+  username: string;
+}
+
+export class SettingsService {
+  async create({ chat, username }: ISettingsCreate) {
+    const settingsRepository = getCustomRepository(SettingsRepository);
+
+    const settings = settingsRepository.create({
+      chat,
+      username,
+    });
+    await settingsRepository.save(settings);
+
+    return settings;
+  }
+}
