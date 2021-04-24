7af28a59573c7fd51e1527e5cfea34fd91acbd85
Bruno Brasil <bruno.brasilf@gmail.com>
2021-04-24 14:26:15 -0300

    feat: save message on repository

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -1,18 +1,27 @@
 import { io } from "../http";
 import { ConnectionsService } from "../services/ConnectionsService";
+import { MessagesService } from "../services/MessagesService";
 import { UsersService } from "../services/UsersService";
+
+interface IParams {
+  text: string;
+  email: string;
+}
 
 io.on("connect", (socket) => {
   const connectionsService = new ConnectionsService();
   const usersService = new UsersService();
+  const messagesService = new MessagesService();
 
   socket.on("client_first_access", async (params) => {
     const socket_id = socket.id;
-    const { text, email } = params;
+    const { text, email } = params as IParams;
+    let user_id = null;
 
     const userExists = await usersService.findByEmail(email);
 
     if (userExists) {
+      user_id = userExists.id;
       const connection = await connectionsService.findByUserId(userExists.id);
 
       if (connection) {
@@ -20,16 +29,22 @@ io.on("connect", (socket) => {
         await connectionsService.create(connection);
       } else {
         await connectionsService.create({
-          user_id: userExists.id,
+          user_id,
           socket_id,
         });
       }
     } else {
       const user = await usersService.create(email);
+      user_id = user.id;
       await connectionsService.create({
-        user_id: user.id,
+        user_id,
         socket_id,
       });
     }
+
+    await messagesService.create({
+      text,
+      user_id,
+    });
   });
 });
