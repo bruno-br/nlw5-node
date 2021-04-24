448e8de8d62ebe93acbaeeffd93312ca8b723d77
Bruno Brasil <bruno.brasilf@gmail.com>
2021-04-24 14:17:30 -0300

    feat: preserve connection if it already exists

--- src/websocket/client.ts
+++ src/websocket/client.ts
@@ -13,10 +13,17 @@ io.on("connect", (socket) => {
     const userExists = await usersService.findByEmail(email);
 
     if (userExists) {
-      await connectionsService.create({
-        user_id: userExists.id,
-        socket_id,
-      });
+      const connection = await connectionsService.findByUserId(userExists.id);
+
+      if (connection) {
+        connection.socket_id = socket_id;
+        await connectionsService.create(connection);
+      } else {
+        await connectionsService.create({
+          user_id: userExists.id,
+          socket_id,
+        });
+      }
     } else {
       const user = await usersService.create(email);
       await connectionsService.create({
