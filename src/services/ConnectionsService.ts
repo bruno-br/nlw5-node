791d64c8bc1140f6b2ff844b366780a7ec2a16b7
Bruno Brasil <bruno.brasilf@gmail.com>
2021-04-24 13:33:59 -0300

    feat: create ConnectionsService

---
+++ src/services/ConnectionsService.ts
@@ -0,0 +1,31 @@
+import { getCustomRepository, Repository } from "typeorm";
+import { ConnectionsRepository } from "../repositories/ConnectionsRepository";
+import { Connection } from "../entities/Connection";
+
+interface IConnectionCreate {
+  user_id: string;
+  socket_id: string;
+  admin_id?: string;
+  id?: string;
+}
+
+export class ConnectionsService {
+  private connectionsRepository: Repository<Connection>;
+
+  constructor() {
+    this.connectionsRepository = getCustomRepository(ConnectionsRepository);
+  }
+
+  async create({ user_id, socket_id, admin_id, id }: IConnectionCreate) {
+    const connection = this.connectionsRepository.create({
+      user_id,
+      socket_id,
+      admin_id,
+      id,
+    });
+
+    await this.connectionsRepository.save(connection);
+
+    return connection;
+  }
+}
